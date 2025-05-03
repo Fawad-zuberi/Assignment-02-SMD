839d07c710ae518646ac53a2289f6108a45c143e
Fawad-zuberi <fawad.zuberi11@gmail.com>
2025-05-03 08:40:24 +0500

    chore:updated root with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:assignment2/BloC/Auth_Bloc.dart';
+import 'package:assignment2/firebase_options.dart';
 import 'package:assignment2/screens/HomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    BlocProvider(
+      create: (context) => AuthBloc(FirebaseAuth.instance),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
